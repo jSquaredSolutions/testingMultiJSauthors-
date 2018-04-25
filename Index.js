c0f28a89b4410cbd5e3096defecdd1ef70202c1e
Jsquared <info@jsquaredsolutions.net>
2018-04-25 15:21:47 -0400

    "test"

--- Index.js
+++ Index.js
@@ -6,7 +6,7 @@ z = x + y;    // Assign the sum of x and y to z
 document.getElementById("demo").innerHTML =
 "The value of z is " + z + ".";
 
-var x, y, z;  // Declare 3 variables
+var x, y, x;  // Declare 3 variables
 x = 5;        // Assign the value 5 to x
 // new hello stuff here
 
